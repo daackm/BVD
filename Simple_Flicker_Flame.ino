9e485ce603a55e43240067c737c504bad0dca6eb
daackm <59622612+daackm@users.noreply.github.com>
2020-05-18 10:31:27 -0500

    Add files via upload

---
+++ Simple_Flicker_Flame.ino
@@ -0,0 +1,22 @@
+// LED Fire Effect
+
+#include "SoftwareSerial.h"
+int ledPin1 = 10;
+int ledPin2 = 9;
+int ledPin3 = 11;
+
+void setup()
+{
+pinMode(ledPin1, OUTPUT);
+pinMode(ledPin2, OUTPUT);
+pinMode(ledPin3, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+Serial.println(millis());
+analogWrite(ledPin1, random(120)+135);
+analogWrite(ledPin2, random(120)+135);
+analogWrite(ledPin3, random(120)+135);
+delay(random(100));
+}
